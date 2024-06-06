2ca9092f571c78a1e6fe5760af37834f104abb04
Orlev <orlev01@gmail.com>
2024-06-06 13:24:28 +0300

    add to server.py waitress

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flaskr import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    serve(app, host="0.0.0.0", port=8000)
