0f9d3c54602bf4ef33ff06251231ba40e7c95f70
Orlev <orlev01@gmail.com>
2024-06-06 21:29:22 +0300

    Fix bug in __init__.py that prevented the name of the stock from being transferred to StockAPI.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,9 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request
+from StockAPI import get_stock_market
+from Stock import Stock
+from StockStatistics import day_statistics
 
 def create_app(test_config = None):
     # create and configure the app
@@ -21,9 +24,22 @@ def create_app(test_config = None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-
+    
     @app.route('/')
-    def hello():
-        return 'hello'
+    @app.route('/index')
+    def index():
+        return render_template('index.html')
+    
+    @app.route('/stock')
+    def get_stock():
+        stock_name = request.args.get('Stock')
+        stock_data = get_stock_market(stock_name)
+        stock_statistics = day_statistics(stock_data)
+        return render_template(
+            "Stock.html",
+            title=Stock.get_name(stock_data),
+            update_day=Stock.get_update_date(stock_data),
+            invest=True if stock_statistics > 0 else False
+        )
+    
     return app
